13d635acb891816619811601eb55630551e586ae
Dawid Dęby <dawid.deby@gmail.com>
2021-03-29 15:45:06 +0200

    Generated User and Tasks chema

--- lib/task_app_web/router.ex
+++ lib/task_app_web/router.ex
@@ -1,5 +1,6 @@
 defmodule TaskAppWeb.Router do
   use TaskAppWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -13,10 +14,22 @@ defmodule TaskAppWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/" do
+    pipe_through :browser
+
+    pow_routes()
+  end
+
+
   scope "/", TaskAppWeb do
     pipe_through :browser
 
     get "/", PageController, :index
+  end
+
+  scope "/", TaskAppWeb do
+    pipe_through :browser
+    resources "/tasks", TaskController
   end
 
   # Other scopes may use custom stacks.
